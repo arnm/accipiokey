3553f56ab474530c9cdb3e7a6a019dec16df5f14
Alexei Nunez <anunez@sparkcognition.com>
2014-02-01 22:26:26 -0600

    Adding loggers

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
-from PySide.QtGui import QApplication
-import sys
+from log.logger import LinuxLogger
 
-class App(QApplication):
-
-    def __init__(self, argv):
-        super(App, self).__init__(argv)
-
-if __name__ == '__main__':
-    app = App(sys.argv)
-    sys.exit(app.exec_())
+LinuxLogger.initialize()
+logger = LinuxLogger.instance()
+logger.log_forever()
