7de6642d964cc0c7755160af7b4486575709ece5
jevi <arnunezm@yahoo.com>
2014-02-26 09:34:26 -0600

    switch to python3 and replaced libs

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from log.logger import LinuxLogger
+from accipiokey.indices import *
+from elasticsearch import Elasticsearch
 
-LinuxLogger.initialize()
-logger = LinuxLogger.instance()
-logger.log_forever()
+es = Elasticsearch(hosts=[{"host": "localhost", "port": 9300}])
+with open('data/words.txt') as data_file:
+    index_str_list(client=es, index='accipiokey_index', doc_type='string_type', str_list=data_file.readlines())
+
+response = es.get(index='accipiokey_index', doc_type='string_type' , id=1)
+print('response', response)
+print('source', response['_source'])
+
